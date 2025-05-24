f16521b32b3daeb707cfaf334484a6753a95b0ca
tmg1981 <moradi6021@gmail.com>
2025-05-24 21:30:28 +0330

    14030303-0930

--- app.py
+++ app.py
@@ -85,7 +85,6 @@ def show_watchlist():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return render_template("watchlist.html", tokens=tokens, last_updated=now)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
